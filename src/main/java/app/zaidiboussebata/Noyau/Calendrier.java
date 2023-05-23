17985f2a764fef8c5596aaa692af442629c1cfd1
ISSAM2411 <116304749+ISSAM2411@users.noreply.github.com>
2023-05-23 20:12:56 +0100

    Add files via upload

--- src/main/java/app/zaidiboussebata/Noyau/Calendrier.java
+++ src/main/java/app/zaidiboussebata/Noyau/Calendrier.java
@@ -1,17 +1,16 @@
-package app.zaidiboussebata.Noyau;
-
+package my_desktop_planner;
 
 import java.util.List;
 
 public class Calendrier {
 
-    public List<SimpleTache> listeTaches ;
-    public List<Creneau_libre> listeCreneau ;
-    public List<Projet> listeProjet;
-
+	public List<SimpleTache> listeTaches ;
+	public List<Creneau_libre> listeCreneau ;
+	public List<Projet> listeProjet;
+    
     public Calendrier() {
-        // TODO Auto-generated constructor stub
-    }
+		// TODO Auto-generated constructor stub
+	}
 
 
 
